9f9777edfa0d2a9fe4279bb09311ea090e0753e6
Dr. Sebestyén-Teleki István <istvan@swissdent.hu>
2018-10-23 15:40:21 +0200

    Adding functionality to test if file exists. If file is symbolic link, it will be deleted. If not, it will be renamed to file.bak

--- install.sh
+++ install.sh
@@ -6,6 +6,21 @@ DOTDIR="$HOME/.dotfiles"
 
 cd $HOME
 
+[ -L $HOME/.gitconfig ] && rm -f $HOME/.gitconfig
+[ -L $HOME/.zshrc ] && rm -f $HOME/.zshrc
+[ -L $HOME/.tmux.conf ] && rm -f $HOME/.tmux.conf
+[ -L $HOME/.vimrc ] && rm -f $HOME/.vimrc
+[ -L $HOME/.oh-my-zsh ] && rm -f $HOME/.oh-my-zsh
+[ -L $HOME/.tmux-themepack ] && rm -f $HOME/.tmux-themepack
+
+
+[ -f $HOME/.gitconfig ] && mv $HOME/.gitconfig $HOME/.gitconfig.bak
+[ -f $HOME/.zshrc ] && mv $HOME/.zshrc $HOME/.zshrc.bak
+[ -f $HOME/.vimrc ] && mv $HOME/.vimrc $HOME/.vimrc.bak
+[ -f $HOME/.tmux.conf ] && mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
+[ -d $HOME/.oh-my-zsh ] && mv $HOME/.oh-my-zsh $HOME/.oh-my-zsh.bak
+[ -d $HOME/.tmux-themepack ] && mv $HOME/.tmux-themepack $HOME/.tmux-themepack.bak
+
 ln -s $DOTDIR/.gitconfig $HOME
 ln -s $DOTDIR/.tmux.conf $HOME
 ln -s $DOTDIR/.vimrc $HOME
