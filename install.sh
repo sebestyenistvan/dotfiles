b8528d0513031835fb19660c801f41538a7fe08e
Dr. Sebestyén-Teleki István <istvan@swissdent.hu>
2018-10-23 14:55:13 +0200

    created installation process for oh-my-zsh and other stuff

--- install.sh
+++ install.sh
@@ -4,8 +4,25 @@
 
 DOTDIR="~/.dotfiles"
 
-ln -s $DOTDIR/.oh-my-zsh $HOME
-ln -s $DOTDIR/.tmux-themepack $HOME
 ln -s $DOTDIR/.tmux.conf $HOME
 ln -s $DOTDIR/.vimrc $HOME
 ln -s $DOTDIR/.zshrc $HOME
+
+cd $HOME
+# INSTALL oh-my-zsh
+# Please install zsh first
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+mv $HOME/.oh-my-zsh $HOME/.dotfiles
+ln -s $HOME/.dotfiles/.oh-my-zsh $HOME
+
+# Add autosuggestion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Add syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install tmux themepack
+# Please install tmux first
+git clone https://github.com/jimeh/tmux-themepack.git ~/.dotfiles/.tmux-themepack
+ln -s $HOME/.dotfiles/.tmux-themepack $HOME
